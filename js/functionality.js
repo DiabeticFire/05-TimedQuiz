668cc7a2e1316094dad5176d03f924adb95b8539
Joseph <ejosephvito@gmail.com>
2019-09-27 21:31:54 -0400

    bugfixes

--- js/functionality.js
+++ js/functionality.js
@@ -6,7 +6,6 @@ let answeredIncorrectly = 0;
 function loadQuestion() {
   // Select Question
   let question = questions[Math.floor(Math.random() * questions.length)];
-  console.log(question);
 
   // Select Answer Choices
   let answerChoices = [];
@@ -14,15 +13,12 @@ function loadQuestion() {
     let element = table[Math.floor(Math.random() * table.length)];
     if (!answerChoices.includes(element)) answerChoices.push(element);
   } while (answerChoices.length < 4);
-  console.log(answerChoices);
 
   // Select Correct Answer
   let correctSlot = Math.floor(Math.random() * 4);
 
   // Display Question
-  $("#question").text(
-    question.replace("ELEMENT", answerChoices[correctSlot].name)
-  );
+  $("#question").text(question.replace("ELEMENT", answerChoices[correctSlot].name));
 
   // Determine which question was asked
   if (question.includes("atomic number")) {
@@ -37,7 +33,7 @@ function loadQuestion() {
     $("#answerChoice4").attr("value", answerChoices[3].atomicNumber);
 
     // Set correct
-    correct = answerChoices[correctSlot].atomicNumber;
+    correct = answerChoices[correctSlot].atomicNumber.toString();
   } else if (question.includes("symbol")) {
     // Display Answer Choices
     $("#answerChoiceLabel1").text(answerChoices[0].symbol);
@@ -51,49 +47,46 @@ function loadQuestion() {
 
     // Set correct
     correct = answerChoices[correctSlot].symbol;
-
-    // Set Timer
-    let remainingTicks = 100;
-    $("#time").css("width", "100%");
-    let timer = setInterval(() => {
-      remainingTicks -= 1;
-      $("#time").css("width", remainingTicks + "%");
-
-      if (remainingTicks === 0) {
-        questionEnded(false);
-      }
-    }, 50);
   }
+  console.log("correct:  " + correct);
+
+  // Set Timer
+  // let remainingTicks = 100;
+  // $("#time").css("width", "100%");
+  // let timer = setInterval(() => {
+  //   remainingTicks -= 1;
+  //   $("#time").css("width", remainingTicks + "%");
+
+  //   if (remainingTicks === 0) {
+  //     questionEnded(false);
+  //   }
+  // }, 200);
 }
 
-$("#form").submit(e => {
+$("#form").submit((e) => {
   e.preventDefault();
+
+  console.log("answered: " + $("input[name='answerChoice']:checked").val());
 
   if ($("input[name='answerChoice']:checked").val() === correct) {
     questionEnded(true);
   } else {
     questionEnded(false);
   }
-
-  questionEnded();
 });
 
 function questionEnded(correctness) {
+  console.log(correctness);
   if (correctness) {
     answeredCorrectly += 1;
     alert("CORRECT!");
   } else {
     answeredIncorrectly += 1;
-    alert("WRONG!");
+    alert("WRONG!sxdfcgvbhxsdfc gvhb");
   }
 
-  if (answeredCorrectly + answeredIncorrectly === 10) {
-    alert(
-      "GAME OVER\nCorrect: " +
-        answeredCorrectly +
-        "\nIncorrect: " +
-        answeredIncorrectly
-    );
+  if (answeredCorrectly + answeredIncorrectly === 5) {
+    alert("GAME OVER\nCorrect: " + answeredCorrectly + "\nIncorrect: " + answeredIncorrectly);
   } else loadQuestion();
 }
 
